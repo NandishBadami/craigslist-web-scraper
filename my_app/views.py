a8aed8358ee80693a38b3b1d85b3600aae972d38
NandishBadami <nandishbadamigoogly@gmail.com>
2026-01-13 16:04:18 +0530

    Set proxies to null

--- my_app/views.py
+++ my_app/views.py
@@ -12,7 +12,10 @@ headers = {
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/120.0.0.0 Safari/537.36"
-    )
+    ),
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.google.com/"
 }
 
 def home(request):
